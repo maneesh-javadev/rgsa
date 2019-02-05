5e452d25731341dd1325db16e0ed4acacf999783
lalall <abc@abc.io>
2019-02-05 13:01:14 +0530

    Bootstrapped application...

---
+++ src/main/java/gov/in/rgsa/RgsaApplication.java
@@ -0,0 +1,14 @@
+package gov.in.rgsa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RgsaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RgsaApplication.class, args);
+    }
+
+}
+
