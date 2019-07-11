15220b1d9e50a9e05ee3aad3c413a705d2057f80
aashish <aashishbarua.epri@gmail.com>
2019-07-11 11:31:51 +0530

    change in ws (user wants state code in the final result)

--- src/main/java/gov/in/rgsa/dto/HundredDaysWebServiceDTO.java
+++ src/main/java/gov/in/rgsa/dto/HundredDaysWebServiceDTO.java
@@ -11,7 +11,7 @@ import org.hibernate.annotations.NamedNativeQuery;
 @NamedNativeQueries({
 	@NamedNativeQuery(name="FETCH_ALL_STATE_DATA",query="SELECT\r\n" + 
 			"ROW_NUMBER () OVER () as s_no, \r\n" + 
-			"null as field_type ,\r\n" + 
+			"null as state_code ,\r\n" + 
 			"null as state_name_english ,\r\n" + 
 			"(select (COALESCE(subtable.total_female_sar_sc,0) + COALESCE(subtable.total_male_sar_sc,0) + COALESCE(subtable.total_female_sar_st,0) + \r\n" + 
 			"COALESCE(subtable.total_male_sar_st,0) + COALESCE(subtable.total_female_sar_oth,0) + COALESCE(subtable.total_male_sar_oth ,0) +\r\n" + 
@@ -53,9 +53,9 @@ import org.hibernate.annotations.NamedNativeQuery;
 			"(select sum(trg_conducted_in_asp_district)  from rgsa.trg_of_hundred_days_program_ch2) as total_trg_conducted_in_asp_district\r\n" + 
 			"\r\n" + 
 			"FROM rgsa.trg_of_hundred_days_program_ch2 LIMIT 1",resultClass = HundredDaysWebServiceDTO.class),
-	@NamedNativeQuery(name="STATEWISE_DATA_FOR_ALL_FIELDS",query="select ROW_NUMBER () OVER () as s_no,null as field_type,s.state_name_english,COALESCE(sum(ch.er_sar_f_sc),0) + COALESCE(sum(ch.er_sar_m_sc),0) + COALESCE(sum(ch.er_sar_f_st),0) + COALESCE(sum(ch.er_sar_m_st),0) + COALESCE(sum(ch.er_sar_f_ot),0) + COALESCE( sum(ch.er_sar_m_ot),0) + COALESCE(sum(ch.er_oth_m_sc),0) + COALESCE(sum(ch.er_oth_f_sc),0) + COALESCE( sum(ch.er_oth_m_st),0) + COALESCE( sum(ch.er_oth_f_st),0) + COALESCE(sum(ch.er_oth_m_ot),0) + COALESCE( sum(ch.er_oth_f_ot),0) as total_er_rep,COALESCE(sum(ch.er_sar_f_sc),0) + COALESCE(sum(ch.er_sar_f_st),0) + COALESCE(sum(ch.er_sar_f_ot),0)  + COALESCE(sum(ch.er_oth_f_sc),0) + COALESCE( sum(ch.er_oth_f_st),0) + COALESCE( sum(ch.er_oth_f_ot),0) as total_women_er_rep ,COALESCE(sum(ch.er_sar_f_sc),0) + COALESCE(sum(ch.er_sar_m_sc),0) + COALESCE(sum(ch.er_oth_m_sc),0) + COALESCE(sum(ch.er_oth_f_sc),0) as total_sc_er_rep,COALESCE(sum(ch.er_sar_f_st),0) + COALESCE(sum(ch.er_sar_m_st),0) + COALESCE( sum(ch.er_oth_m_st),0) + COALESCE( sum(ch.er_oth_f_st),0) as total_st_er_rep,COALESCE(sum(er_in_aspirational_district),0) as total_er_in_aspirational_district,COALESCE(sum(fun_in_aspirational_district),0) as total_fun_in_aspirational_district,COALESCE(sum(no_of_training_conducted),0) as total_no_of_training_conducted,\r\n" + 
+	@NamedNativeQuery(name="STATEWISE_DATA_FOR_ALL_FIELDS",query="select ROW_NUMBER () OVER () as s_no,s.state_code,s.state_name_english,COALESCE(sum(ch.er_sar_f_sc),0) + COALESCE(sum(ch.er_sar_m_sc),0) + COALESCE(sum(ch.er_sar_f_st),0) + COALESCE(sum(ch.er_sar_m_st),0) + COALESCE(sum(ch.er_sar_f_ot),0) + COALESCE( sum(ch.er_sar_m_ot),0) + COALESCE(sum(ch.er_oth_m_sc),0) + COALESCE(sum(ch.er_oth_f_sc),0) + COALESCE( sum(ch.er_oth_m_st),0) + COALESCE( sum(ch.er_oth_f_st),0) + COALESCE(sum(ch.er_oth_m_ot),0) + COALESCE( sum(ch.er_oth_f_ot),0) as total_er_rep,COALESCE(sum(ch.er_sar_f_sc),0) + COALESCE(sum(ch.er_sar_f_st),0) + COALESCE(sum(ch.er_sar_f_ot),0)  + COALESCE(sum(ch.er_oth_f_sc),0) + COALESCE( sum(ch.er_oth_f_st),0) + COALESCE( sum(ch.er_oth_f_ot),0) as total_women_er_rep ,COALESCE(sum(ch.er_sar_f_sc),0) + COALESCE(sum(ch.er_sar_m_sc),0) + COALESCE(sum(ch.er_oth_m_sc),0) + COALESCE(sum(ch.er_oth_f_sc),0) as total_sc_er_rep,COALESCE(sum(ch.er_sar_f_st),0) + COALESCE(sum(ch.er_sar_m_st),0) + COALESCE( sum(ch.er_oth_m_st),0) + COALESCE( sum(ch.er_oth_f_st),0) as total_st_er_rep,COALESCE(sum(er_in_aspirational_district),0) as total_er_in_aspirational_district,COALESCE(sum(fun_in_aspirational_district),0) as total_fun_in_aspirational_district,COALESCE(sum(no_of_training_conducted),0) as total_no_of_training_conducted,\r\n" + 
 			"COALESCE(sum(trg_conducted_in_asp_district),0) as total_trg_conducted_in_asp_district\r\n" + 
-			"from rgsa.trg_of_hundred_days_program_ch2 ch right join lgd.state s on ch.state_code=s.state_code group by s.state_name_english order by s.state_name_english",resultClass = HundredDaysWebServiceDTO.class)
+			"from rgsa.trg_of_hundred_days_program_ch2 ch right join lgd.state s on ch.state_code=s.state_code group by s.state_name_english, s.state_code order by s.state_name_english",resultClass = HundredDaysWebServiceDTO.class)
 })
 public class HundredDaysWebServiceDTO {
 
@@ -63,8 +63,8 @@ public class HundredDaysWebServiceDTO {
 	@Column(name="s_no")
 	private Integer sNo;
 	
-	@Column(name="field_type")
-	private String fieldType;
+	@Column(name="state_code")
+	private String stateCode;
 	
 	@Column(name="state_name_english")
 	private String stateName;
@@ -173,14 +173,12 @@ public class HundredDaysWebServiceDTO {
 		this.noOfTrainingConductedAspDistrict = noOfTrainingConductedAspDistrict;
 	}
 
-	public String getFieldType() {
-		return fieldType;
+	public String getStateCode() {
+		return stateCode;
 	}
 
-	public void setFieldType(String fieldType) {
-		this.fieldType = fieldType;
+	public void setStateCode(String stateCode) {
+		this.stateCode = stateCode;
 	}
-	
-	
 	
 }
